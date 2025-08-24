45c7ec468116cb238f85580556f0aeb1dc044af6
Renggars <renggarendy22@gmail.com>
2025-08-24 14:55:55 +0700

    refactor: Update OrderPanel UI with new transaction summary

--- lib/features/presentation/widgets/order_panel.dart
+++ lib/features/presentation/widgets/order_panel.dart
@@ -1,3 +1,4 @@
+// lib/features/pos/presentation/widgets/order_panel.dart
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:intl/intl.dart';
@@ -13,125 +14,250 @@ class OrderPanel extends StatelessWidget {
     return Container(
       color: AppColors.white,
       padding: const EdgeInsets.all(24),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          const Text(
-            'Orders #',
-            style: TextStyle(
-              fontSize: 30,
-              fontWeight: FontWeight.bold,
-              color: AppColors.primary,
-            ),
-          ),
-          const SizedBox(height: 10),
-          const Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      child: BlocBuilder<OrderBloc, OrderState>(
+        builder: (context, state) {
+          double subTotal =
+              state.orderItems.fold(0, (sum, item) => sum + item.totalPrice);
+          // Asumsi nilai tetap untuk demo
+          const double serviceFeePercentage = 0.05; // 5%
+          const double taxPercentage = 0.10; // 10%
+          const double discountAmount = 0; // Asumsi diskon Rp 0
+
+          double serviceFee = subTotal * serviceFeePercentage;
+          double tax = subTotal * taxPercentage;
+          double grandTotal = subTotal + serviceFee + tax - discountAmount;
+
+          return Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Text(
-                'Item',
+              const Text(
+                'Orders #',
                 style: TextStyle(
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold,
                   color: AppColors.primary,
-                  fontSize: 20,
                 ),
               ),
-              Text(
-                'Qty',
-                style: TextStyle(
-                  color: AppColors.primary,
-                  fontSize: 20,
-                ),
-              ),
-              Text(
-                'Price',
-                style: TextStyle(
-                  color: AppColors.primary,
-                  fontSize: 20,
-                ),
-              ),
-            ],
-          ),
-          const SizedBox(height: 10),
-          const Divider(thickness: 1),
-          const SizedBox(height: 10),
-          Expanded(
-            child: BlocBuilder<OrderBloc, OrderState>(
-              builder: (context, state) {
-                if (state.orderItems.isEmpty) {
-                  return const Center(
+              const SizedBox(height: 10),
+              const Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Expanded(
+                    flex: 4,
                     child: Text(
-                      'No orders yet',
-                      style: TextStyle(color: AppColors.fontGrey),
+                      'Item',
+                      style: TextStyle(
+                        color: AppColors.primary,
+                        fontSize: 20,
+                      ),
                     ),
-                  );
-                }
-                return ListView.builder(
-                  padding: EdgeInsets.zero,
-                  itemCount: state.orderItems.length,
-                  itemBuilder: (context, index) {
-                    return OrderListItem(item: state.orderItems[index]);
-                  },
-                );
-              },
-            ),
-          ),
-          const SizedBox(height: 20),
-          const Divider(thickness: 1, height: 10),
-          _buildPaymentMethods(),
-          const SizedBox(height: 20),
-          SizedBox(
-            width: double.infinity,
-            child: ElevatedButton(
-              onPressed: () {},
-              style: ElevatedButton.styleFrom(
-                padding: const EdgeInsets.symmetric(vertical: 24),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(14),
-                ),
+                  ),
+                  Expanded(
+                    flex: 2,
+                    child: Text(
+                      'Qty',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        color: AppColors.primary,
+                        fontSize: 20,
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    flex: 3,
+                    child: Text(
+                      'Price',
+                      textAlign: TextAlign.right,
+                      style: TextStyle(
+                        color: AppColors.primary,
+                        fontSize: 20,
+                      ),
+                    ),
+                  ),
+                ],
               ),
-              child: const Text(
-                'Payment',
-                style: TextStyle(
-                  fontSize: 18,
-                ),
+              const SizedBox(height: 10),
+              const Divider(thickness: 1),
+              const SizedBox(height: 10),
+              Expanded(
+                child: state.orderItems.isEmpty
+                    ? const Center(
+                        child: Text(
+                          'No orders yet',
+                          style: TextStyle(color: AppColors.fontGrey),
+                        ),
+                      )
+                    : ListView.builder(
+                        padding: EdgeInsets.zero,
+                        itemCount: state.orderItems.length,
+                        itemBuilder: (context, index) {
+                          return OrderListItem(item: state.orderItems[index]);
+                        },
+                      ),
               ),
-            ),
-          )
-        ],
+              const SizedBox(height: 20),
+              _buildTransactionOptions(),
+              const SizedBox(height: 20),
+              _buildTransactionSummary(
+                subTotal: subTotal,
+                serviceFee: serviceFee,
+                tax: tax,
+                discount: discountAmount,
+                grandTotal: grandTotal,
+              ),
+              const SizedBox(height: 20),
+              SizedBox(
+                width: double.infinity,
+                child: ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: AppColors.primary,
+                    padding: const EdgeInsets.symmetric(vertical: 24),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(14),
+                    ),
+                  ),
+                  child: const Text(
+                    'Lanjutkan Pembayaran',
+                    style: TextStyle(
+                      fontSize: 18,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              )
+            ],
+          );
+        },
       ),
     );
   }
 
-  Widget _buildPaymentMethods() {
-    return GridView.count(
-      shrinkWrap: true, // biar tinggi grid ngikut konten
-      physics: const NeverScrollableScrollPhysics(), // disable scroll grid
-      crossAxisCount: 3, // 3 kolom
-      crossAxisSpacing: 24, // jarak antar kolom
-      childAspectRatio: 1.6, // rasio lebar : tinggi, bisa disesuaikan
+  Widget _buildTransactionOptions() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
       children: [
-        _paymentMethodButton(Icons.money, 'CASH'),
-        _paymentMethodButton(Icons.qr_code, 'QR'),
-        _paymentMethodButton(Icons.credit_card, 'TRANSFER'),
+        _buildOptionCard(
+          icon: Icons.discount,
+          label: 'Diskon',
+          onTap: () {
+            // Aksi untuk menerapkan diskon
+          },
+        ),
+        _buildOptionCard(
+          icon: Icons.percent,
+          label: 'Pajak PB1',
+          onTap: () {
+            // Aksi untuk menerapkan pajak
+          },
+        ),
+        _buildOptionCard(
+          icon: Icons.room_service,
+          label: 'Layanan',
+          onTap: () {
+            // Aksi untuk menerapkan biaya layanan
+          },
+        ),
       ],
     );
   }
 
-  Widget _paymentMethodButton(IconData icon, String label) {
-    return OutlinedButton(
-      onPressed: () {},
-      style: OutlinedButton.styleFrom(
-        side: const BorderSide(color: AppColors.primary),
-        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(14)),
+  Widget _buildOptionCard(
+      {required IconData icon,
+      required String label,
+      required VoidCallback onTap}) {
+    return Expanded(
+      child: Card(
+        elevation: 2,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(12),
+        ),
+        child: InkWell(
+          onTap: onTap,
+          borderRadius: BorderRadius.circular(12),
+          child: Padding(
+            padding: const EdgeInsets.symmetric(vertical: 16.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Icon(icon, color: AppColors.primary, size: 28),
+                const SizedBox(height: 6),
+                Text(
+                  label,
+                  style: const TextStyle(
+                    color: AppColors.fontGrey,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
       ),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
+    );
+  }
+
+  Widget _buildTransactionSummary({
+    required double subTotal,
+    required double serviceFee,
+    required double tax,
+    required double discount,
+    required double grandTotal,
+  }) {
+    final formatCurrency = NumberFormat.currency(
+      locale: 'id_ID',
+      symbol: 'Rp. ',
+      decimalDigits: 0,
+    );
+
+    return Column(
+      children: [
+        _buildSummaryRow(
+            'Pajak PB1',
+            '${(tax / subTotal * 100).toStringAsFixed(0)} %',
+            formatCurrency.format(tax)),
+        _buildSummaryRow(
+            'Layanan',
+            '${(serviceFee / subTotal * 100).toStringAsFixed(0)} %',
+            formatCurrency.format(serviceFee)),
+        _buildSummaryRow('Diskon', '', formatCurrency.format(discount)),
+        const Divider(thickness: 1, height: 20),
+        _buildSummaryRow('Sub total', '', formatCurrency.format(grandTotal),
+            isBold: true),
+      ],
+    );
+  }
+
+  Widget _buildSummaryRow(String label, String percentage, String value,
+      {bool isBold = false}) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 4.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-          Icon(icon, color: AppColors.primary, size: 32),
-          const SizedBox(height: 6), // jarak icon dan label
           Text(
             label,
-            style: const TextStyle(color: AppColors.primary, fontSize: 16),
+            style: TextStyle(
+              fontSize: 18,
+              fontWeight: isBold ? FontWeight.bold : FontWeight.normal,
+              color: isBold ? AppColors.primary : AppColors.fontGrey,
+            ),
+          ),
+          Text(
+            percentage,
+            style: const TextStyle(
+              fontSize: 16,
+              color: AppColors.fontGrey,
+            ),
+          ),
+          Text(
+            value,
+            style: TextStyle(
+              fontSize: 18,
+              fontWeight: isBold ? FontWeight.bold : FontWeight.normal,
+              color: isBold ? AppColors.primary : AppColors.fontGrey,
+            ),
           ),
         ],
       ),
@@ -139,6 +265,7 @@ class OrderPanel extends StatelessWidget {
   }
 }
 
+// Class OrderListItem dan AppColors tetap sama seperti sebelumnya
 class OrderListItem extends StatelessWidget {
   final OrderItem item;
   const OrderListItem({super.key, required this.item});
@@ -155,77 +282,54 @@ class OrderListItem extends StatelessWidget {
       padding: const EdgeInsets.symmetric(
         vertical: 8.0,
       ),
-      child: Column(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Expanded(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      item.product.name,
-                      style: const TextStyle(
-                        fontSize: 22,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    Text(
-                      formatCurrency.format(item.product.price),
-                      style: const TextStyle(
-                        fontSize: 20,
-                        color: AppColors.fontGrey,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-
-              // Tengah
-              _buildQuantitySelector(context),
-              const SizedBox(width: 30),
-
-              // Kanan
-              SizedBox(
-                child: Text(
-                  formatCurrency.format(item.totalPrice),
-                  textAlign: TextAlign.end,
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  item.product.name,
                   style: const TextStyle(
-                    fontSize: 20,
+                    fontSize: 22,
                     fontWeight: FontWeight.bold,
                   ),
                 ),
-              ),
-            ],
-          ),
-          const SizedBox(height: 12),
-          TextField(
-            style: const TextStyle(fontSize: 20, color: AppColors.fontGrey),
-            decoration: InputDecoration(
-              hintText: 'Notes',
-              hintStyle: const TextStyle(color: AppColors.fontGrey),
-              enabledBorder: OutlineInputBorder(
-                // border normal
-                borderRadius: BorderRadius.circular(8),
-                borderSide: const BorderSide(color: Colors.grey, width: 1),
-              ),
-              focusedBorder: OutlineInputBorder(
-                // border saat fokus
-                borderRadius: BorderRadius.circular(8),
-                borderSide: const BorderSide(color: Colors.grey, width: 2),
-              ),
-              contentPadding:
-                  const EdgeInsets.symmetric(horizontal: 12, vertical: 12),
+                Text(
+                  formatCurrency.format(item.product.price),
+                  style: const TextStyle(
+                    fontSize: 20,
+                    color: AppColors.fontGrey,
+                  ),
+                ),
+              ],
             ),
-          )
+          ),
+
+          // Tengah (Quantity Selector)
+          _buildQuantitySelector(context, item),
+          const SizedBox(width: 30),
+
+          // Kanan (Total Price)
+          SizedBox(
+            width: 120,
+            child: Text(
+              formatCurrency.format(item.totalPrice),
+              textAlign: TextAlign.end,
+              style: const TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
         ],
       ),
     );
   }
 
-  Widget _buildQuantitySelector(BuildContext context) {
+  Widget _buildQuantitySelector(BuildContext context, OrderItem item) {
     return Container(
       decoration: BoxDecoration(
         color: AppColors.secondary,
@@ -241,8 +345,10 @@ class OrderListItem extends StatelessWidget {
             constraints: const BoxConstraints(),
             padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 4),
           ),
-          Text(item.quantity.toString(),
-              style: const TextStyle(fontWeight: FontWeight.bold)),
+          Text(
+            item.quantity.toString(),
+            style: const TextStyle(fontWeight: FontWeight.bold),
+          ),
           IconButton(
             icon: const Icon(Icons.add, size: 16, color: AppColors.primary),
             onPressed: () {
